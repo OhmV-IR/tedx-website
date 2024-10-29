62f572df4f5548bbacbe23118766142d23a14aa5
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-10-29 11:21:50 -0400

    Added site menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Homepage from './Homepage';
 import Schedule from './Schedule';
 import Sponsors from './Sponsors';
@@ -7,9 +8,40 @@ import Blog from './Blog';
 import About from './About';
 import Speakers from './Speakers';
 import PageNotFound from './PageNotFound';
+import { IconInfoSquare, IconNotebook, IconPhoto, IconCurrencyDollar, IconCalendarEvent, IconUsers, IconMenu2 } from "@tabler/icons-react";
+import './App.css';
 
 function App() {
+    const [siteMenuEnabled, setSiteMenuEnabled] = useState(false)
     return (
+        <div id="approot">
+        <IconMenu2 stroke={2} size={44} id="menuIcon" onClick={ToggleSiteMenu}></IconMenu2>
+        <div id="siteMenu">
+            <a href="/about">
+            <IconInfoSquare id="aboutIcon"></IconInfoSquare>
+            <h3 id="aboutLabel">About</h3>
+            </a>
+            <a href="/blog">
+            <IconNotebook id="blogIcon"></IconNotebook>
+            <h3 id="blogLabel">Blog</h3>
+            </a>
+            <a href="/media">
+            <IconPhoto id="mediaIcon"></IconPhoto>
+            <h3 id="mediaLabel">Media</h3>
+            </a>
+            <a href="/sponsors">
+            <IconCurrencyDollar id="sponsorsIcon"></IconCurrencyDollar>
+            <h3 id="sponsorsLabel">Sponsors</h3>
+            </a>
+            <a href="/schedule">
+            <IconCalendarEvent id="scheduleIcon"></IconCalendarEvent>
+            <h3 id="scheduleLabel">Schedule</h3>
+            </a>
+            <a href="/speakers">
+            <IconUsers id="speakersIcon"></IconUsers>
+            <h3 id="speakersLabel">Speakers</h3>
+            </a>
+        </div>
         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<Homepage></Homepage>}></Route>
@@ -22,6 +54,16 @@ function App() {
                 <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
             </Routes>
         </BrowserRouter>
+        </div>
     )
+    function ToggleSiteMenu(){
+        if(siteMenuEnabled === true){
+            document.getElementById("siteMenu").style.display = "none";
+        }
+        else{
+            document.getElementById("siteMenu").style.display = "block";
+        }
+        setSiteMenuEnabled(!siteMenuEnabled);
+    }
 }
 export default App;
