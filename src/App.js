e9a0628267efa9fb2341e7c7bb6bae69313c418a
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-11-05 08:27:57 -0500

    Added menu slide in animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState} from 'react';
 import Homepage from './Homepage';
 import Schedule from './Schedule';
 import Sponsors from './Sponsors';
@@ -8,7 +8,7 @@ import Blog from './Blog';
 import About from './About';
 import Speakers from './Speakers';
 import PageNotFound from './PageNotFound';
-import { IconInfoSquare, IconNotebook, IconPhoto, IconCurrencyDollar, IconCalendarEvent, IconUsers, IconMenu2, IconX} from "@tabler/icons-react";
+import { IconInfoSquare, IconNotebook, IconPhoto, IconCurrencyDollar, IconCalendarEvent, IconUsers, IconMenu2, IconX } from "@tabler/icons-react";
 import './App.css';
 
 function App() {
@@ -24,25 +24,25 @@ function App() {
             <IconInfoSquare id="aboutIcon" color="#EB0028"></IconInfoSquare>
             <h3 id="aboutLabel">About</h3>
             </a>
-            <a href="/blog">
-            <IconNotebook id="blogIcon" color="#EB0028"></IconNotebook>
-            <h3 id="blogLabel">Blog</h3>
-            </a>
             <a href="/media">
             <IconPhoto id="mediaIcon" color="#EB0028"></IconPhoto>
             <h3 id="mediaLabel">Media</h3>
+            </a>
+            <a href="/blog">
+            <IconNotebook id="blogIcon" color="#EB0028"></IconNotebook>
+            <h3 id="blogLabel">Blog</h3>
             </a>
             <a href="/sponsors">
             <IconCurrencyDollar id="sponsorsIcon" color="#EB0028"></IconCurrencyDollar>
             <h3 id="sponsorsLabel">Sponsors</h3>
             </a>
-            <a href="/schedule">
-            <IconCalendarEvent id="scheduleIcon" color="#EB0028"></IconCalendarEvent>
-            <h3 id="scheduleLabel">Schedule</h3>
-            </a>
             <a href="/speakers">
             <IconUsers id="speakersIcon" color="#EB0028"></IconUsers>
             <h3 id="speakersLabel">Speakers</h3>
+            </a>
+            <a href="/schedule">
+            <IconCalendarEvent id="scheduleIcon" color="#EB0028"></IconCalendarEvent>
+            <h3 id="scheduleLabel">Schedule</h3>
             </a>
         </div>
         <BrowserRouter>
@@ -61,10 +61,17 @@ function App() {
     )
     function ToggleSiteMenu(){
         if(siteMenuEnabled === true){
-            document.getElementById("siteMenu").style.display = "none";
+            let element = document.getElementById("siteMenu");
+            element.style.display = "none";
         }
         else{
-            document.getElementById("siteMenu").style.display = "block";
+            let element = document.getElementById("siteMenu");
+            element.style.display = "block";
+            for(let i = 0; i < element.children.length; i++){
+                let currentMenuItem = element.children.item(i);
+                currentMenuItem.children.item(0).style.animation = "menu-open".concat(i.toString(), "icon", " 0.75s linear 0s 1 normal");
+                currentMenuItem.children.item(1).style.animation = "menu-open".concat(i.toString(), "label", " 0.75s linear 0s 1 normal");
+            }
         }
         setSiteMenuEnabled(!siteMenuEnabled);
     }
