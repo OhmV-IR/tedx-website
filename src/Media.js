d04c38aed43da5b07573517e6c2a0fda7ac822d9
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-12-20 13:27:26 -0500

    Add media (#11)
    * Added carousel for event photos and team photos(need to resize headshots still to make them a consistent size)
    
    * resized headshots
    
    * Made square headshot images and added carousel for about page
    
    * Added header to carousel

--- src/Media.js
+++ src/Media.js
@@ -1,6 +1,43 @@
-function Media(){
-    return(
-        <h1>Media</h1>
+import './Media.css'
+function Media() {
+    return (
+        <div id="mediaRoot">
+        <h1 id="carouselHeader">Event photos</h1>
+            <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="1"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="2"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="3"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="4"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" alt="" src="https://www.picsum.photos/1920/1080" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" alt="" src="https://www.picsum.photos/1920/1080" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" alt="" src="https://www.picsum.photos/1920/1080" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" alt="" src="https://www.picsum.photos/1920/1080" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" alt="" src="https://www.picsum.photos/1920/1080" />
+                    </div>
+                </div>
+                <a class="carousel-control-prev" data-bs-target="#carousel" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </a>
+                <a class="carousel-control-next" data-bs-target="#carousel" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </a>
+            </div>
+        </div>
     )
 }
 export default Media;
