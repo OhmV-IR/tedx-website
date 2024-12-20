a2f119c748929af7d0cdb8b3400c0e01a1dc7c05
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-12-20 13:01:20 -0500

    Changed class and id names to match speaker page

--- src/Speakers.js
+++ src/Speakers.js
@@ -5,8 +5,8 @@ function Speakers() {
         <div>
         <div className="row row-deck">
             <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company1Card">
-                    <div id="companyImg1" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
+                <div className="card card-lg speakerCard" id="speaker1Card">
+                    <div id="speakerImg1" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
                     <div className="card-header">
                         <div className="card-title">
                             Speaker Name
@@ -15,19 +15,19 @@ function Speakers() {
                     <div className="card-body">
                         Here lies a blurb about the speaker, maybe where they work, what they've done or any other information that would be cool to know.
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                    <div className="d-lg-block speakerSocialFooter">
+                        <a href="#" className="speakerSocialLink"><IconWorldWww></IconWorldWww></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandInstagram></IconBrandInstagram></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandFacebook></IconBrandFacebook></a>
+                        <a href="#" className="speakerSocialLink speakerSocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandX></IconBrandX></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandTiktok></IconBrandTiktok></a>
                     </div>
                 </div>
             </div>
             <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company2Card">
-                    <div id="companyImg2" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
+                <div className="card card-lg speakerCard" id="speaker2Card">
+                    <div id="speakerImg2" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
                     <div className="card-header">
                         <div className="card-title">
                             Speaker Name 2
@@ -36,19 +36,19 @@ function Speakers() {
                     <div className="card-body">
                         Here lies a blurb about the speaker, maybe where they work, what they've done or any other information that would be cool to know.
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                    <div className="d-lg-block speakerSocialFooter">
+                        <a href="#" className="speakerSocialLink"><IconWorldWww></IconWorldWww></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandInstagram></IconBrandInstagram></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandFacebook></IconBrandFacebook></a>
+                        <a href="#" className="speakerSocialLink speakerSocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandX></IconBrandX></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandTiktok></IconBrandTiktok></a>
                     </div>
                 </div>
             </div>
             <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company3Card">
-                    <div id="companyImg3" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
+                <div className="card card-lg speakerCard" id="speaker3Card">
+                    <div id="speakerImg3" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
                     <div className="card-header">
                         <div className="card-title">
                             Speaker Name 3
@@ -57,21 +57,21 @@ function Speakers() {
                     <div className="card-body">
                         Here lies a blurb about the speaker, maybe where they work, what they've done or any other information that would be cool to know.
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                    <div className="d-lg-block speakerSocialFooter">
+                        <a href="#" className="speakerSocialLink"><IconWorldWww></IconWorldWww></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandInstagram></IconBrandInstagram></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandFacebook></IconBrandFacebook></a>
+                        <a href="#" className="speakerSocialLink speakerSocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandX></IconBrandX></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandTiktok></IconBrandTiktok></a>
                     </div>
                 </div>
             </div>
         </div>
         <div className="row row-deck">
             <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company4Card">
-                    <div id="companyImg4" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
+                <div className="card card-lg speakerCard" id="speaker4Card">
+                    <div id="speakerImg4" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
                     <div className="card-header">
                         <div className="card-title">
                             Speaker Name 4
@@ -80,19 +80,19 @@ function Speakers() {
                     <div className="card-body">
                         Here lies a blurb about the speaker, maybe where they work, what they've done or any other information that would be cool to know.
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                    <div className="d-lg-block speakerSocialFooter">
+                        <a href="#" className="speakerSocialLink"><IconWorldWww></IconWorldWww></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandInstagram></IconBrandInstagram></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandFacebook></IconBrandFacebook></a>
+                        <a href="#" className="speakerSocialLink speakerSocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandX></IconBrandX></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandTiktok></IconBrandTiktok></a>
                     </div>
                 </div>
             </div>
             <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company5Card">
-                    <div id="companyImg5" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
+                <div className="card card-lg speakerCard" id="speaker5Card">
+                    <div id="speakerImg5" className="img-responsive img-responsive-1x1 card-img-top speakerCardImage"></div>
                     <div className="card-header">
                         <div className="card-title">
                             Speaker Name 5
@@ -101,19 +101,19 @@ function Speakers() {
                     <div className="card-body">
                         Here lies a blurb about the speaker, maybe where they work, what they've done or any other information that would be cool to know.
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                    <div className="d-lg-block speakerSocialFooter">
+                        <a href="#" className="speakerSocialLink"><IconWorldWww></IconWorldWww></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandInstagram></IconBrandInstagram></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandFacebook></IconBrandFacebook></a>
+                        <a href="#" className="speakerSocialLink speakerSocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandX></IconBrandX></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandTiktok></IconBrandTiktok></a>
                     </div>
                 </div>
             </div>
             <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company6Card">
-                    <div id="companyImg6" className="img-responsive img-responsive-1x1 card-img-top"></div>
+                <div className="card card-lg speakerCard" id="speaker6Card">
+                    <div id="speakerImg6" className="img-responsive img-responsive-1x1 card-img-top"></div>
                     <div className="card-header">
                         <div className="card-title">
                             Speaker Name 6
@@ -122,13 +122,13 @@ function Speakers() {
                     <div className="card-body">
                         Here lies a blurb about the speaker, maybe where they work, what they've done or any other information that would be cool to know.
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                    <div className="d-lg-block speakerSocialFooter">
+                        <a href="#" className="speakerSocialLink"><IconWorldWww></IconWorldWww></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandInstagram></IconBrandInstagram></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandFacebook></IconBrandFacebook></a>
+                        <a href="#" className="speakerSocialLink speakerSocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandX></IconBrandX></a>
+                        <a href="#" className="speakerSocialLink"><IconBrandTiktok></IconBrandTiktok></a>
                     </div>
                 </div>
             </div>
