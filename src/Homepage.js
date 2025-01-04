6221ce9aee96ffcd4f9575da6ee27c13cfac42ca
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-04 00:02:13 -0500

    Added required about text to homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,19 @@
+import './Homepage.css';
 function Homepage() {
   return (
     <div id="homepage-root">
-      
+      <div id="about-small">
+        <strong><h1 id="aboutTEDxHeader">
+          What is TEDx?
+        </h1></strong>
+        <h3 id="aboutTEDxText">
+        In the spirit of ideas worth spreading, TED has created a program called TEDx. 
+        TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is called TEDxCentreville Chicopee Youth
+        , where x = independently organized TED event. 
+        At our TEDxCentreville Chicopee Youth event, TEDTalks video and live speakers will combine to spark deep discussion and connection in a small group. 
+        The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.
+        </h3>
+      </div>
     </div>
   );
 }
