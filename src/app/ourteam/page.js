daa76e76819770ec8db81d8e3d7d46f49a8cdf1b
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-23 14:51:04 -0500

    Fixed ourteam page scrolling and menu button is now clickable again

--- src/app/ourteam/page.js
+++ src/app/ourteam/page.js
@@ -6,14 +6,9 @@ export default function Page() {
             <link rel="preconnect" href="https://fonts.googleapis.com"></link>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
             <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet"></link>
-            <style jsx>{`#ourTeamRoot {
-    background-color: #080130 !important;
-    position: absolute;
-    left: 0vw;
-    top: 10vh;
-    width: 100vw;
-    height: 225vh;
-    z-index: -1;
+            <style jsx>{`
+#meetTeamPost {
+    background-color: #080130;
 }
 img {
 overflow-y: hidden;
@@ -30,55 +25,35 @@ overflow-y: hidden;
     color: white;
     position: absolute;
     left: 20vw;
-    top: 25vh;
+    top: 40vh;
     text-align: center;
 }
-#teamPhotoAdrien {
-    position: absolute;
-    left: 0vw;
-    top: -14vh;
-    scale: 0.5;
+.meetTeamPhoto {
+    position: relative;
+    margin-top: 40vh;
+    margin-left: 4vw;
 }
-#teamPhotoDiya {
-    position: absolute;
-    left: 40vw;
-    top: -14vh;
-    scale: 0.5;
-}
-#teamPhotoAfeef {
-    position: absolute;
-    left: 0vw;
-    top: 55vh;
-    scale: 0.5;
-}
-#teamPhotoAlina {
-    position: absolute;
-    left: 40vw;
-    top: 55vh;
-    scale: 0.5;
-}
-#teamPhotoKunsh {
-    position: absolute;
-    left: 0vw;
-    top: 123vh;
-    scale: 0.5;
-}
-#teamPhotoJoydrutt {
-    position: absolute;
-    left: 40vw;
-    top: 123vh;
-    scale: 0.5;
+#background {
+    background-color: #080130;
 }
 * {
     overflow-x: hidden !important;
 }`}</style>
-            <h1 id="pageTitle">MEET THE TEAM</h1>
-            <img id="teamPhotoDiya" src="/meetTheTeamDiya.png" alt="Diya Rathee, Licensee/Organizer of the event"></img>
-            =        <img id="teamPhotoAdrien" src="/meetTheTeamAdrien.png" alt="Adrien Borudeaux, Website manager of the event"></img>
-            <img id="teamPhotoAfeef" src="/meetTheTeamAfeef.png" alt="Afeef Imam, Editorial and Marketing manager of the event"></img>
-            <img id="teamPhotoAlina" src="/meetTheTeamAlina.png" alt="Alina Feng, Event manager of the event"></img>
-            <img id="teamPhotoKunsh" src="/meetTheTeamKunsh.png" alt="Kunsh Sood, Sponsorships manager of the event"></img>
-            <img id="teamPhotoJoydrutt" src="/meetTheTeamJoydrutt.png" alt="Joydrutt Chakraborty, Operations member of the event"></img>
+            <div id="background" tabIndex={-25}>
+                <h1 id="pageTitle" tabIndex={-10}>MEET THE TEAM</h1>
+                <div className="d-flex align-self-center">
+                    <img className="meetTeamPhoto" src="/meetTheTeamDiya.png" alt="Diya Rathee, Licensee/Organizer of the event"></img>
+                    <img className="meetTeamPhoto" src="/meetTheTeamAdrien.png" alt="Adrien Borudeaux, Website manager of the event"></img>
+                </div>
+                <div className="d-flex align-self-center">
+                    <img className="meetTeamPhoto" src="/meetTheTeamAfeef.png" alt="Afeef Imam, Editorial and Marketing manager of the event"></img>
+                    <img className="meetTeamPhoto" src="/meetTheTeamAlina.png" alt="Alina Feng, Event manager of the event"></img>
+                </div>
+                <div className="d-flex align-self-center">
+                    <img className="meetTeamPhoto" src="/meetTheTeamKunsh.png" alt="Kunsh Sood, Sponsorships manager of the event"></img>
+                    <img className="meetTeamPhoto" src="/meetTheTeamJoydrutt.png" alt="Joydrutt Chakraborty, Operations member of the event"></img>
+                </div>
+            </div>
         </div>
     )
 }
