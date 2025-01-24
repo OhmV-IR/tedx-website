80d6fd90bb0e08b7294608f3a5bee906d6eaa432
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-24 15:28:57 -0500

    Fix sponsor card icons (#37)

--- src/app/sponsors/page.js
+++ src/app/sponsors/page.js
@@ -1,9 +1,9 @@
 "use client"
-import { IconWorldWww, IconBrandInstagram, IconBrandFacebook, IconBrandYoutube, IconBrandX, IconBrandTiktok } from "@tabler/icons-react";
-export default function Page(){
-    return(
+import { IconWorldWww, IconBrandInstagram, IconBrandFacebook, IconBrandYoutube, IconBrandX, IconBrandTiktok, IconBrandLinkedin } from "@tabler/icons-react";
+export default function Page() {
+    return (
         <div className="divRoot">
-        <style jsx>{`#companyImg1 {
+            <style jsx>{`#companyImg1 {
     background-image: url("https://www.picsum.photos/1920/1080") !important;
 }
 #companyImg2 {
@@ -33,158 +33,148 @@ export default function Page(){
     margin-left: 3vw;
     margin-top: 10vh;
 }
-.companySocialFooter {
+.socialFooterPadding {
     position: relative;
-    margin-top: -4vh;
-    margin-bottom: 2vh;
+    margin-bottom: 4vh;
+    width: 100%;
 }
-.companySocialLink {
-    position: relative;
-    margin-left: 2.5vw;
+.socialLinkLeftSide {
+    margin-right: 3vw;
+}
+.socialLinkRightSide {
+    margin-left: 3vw;
 }
 `}</style>
-        <div className="row row-deck">
-            <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company1Card">
-                    <div id="companyImg1" className="img-responsive img-responsive-16x9 card-img-top"></div>
-                    <div className="card-header">
-                        <div className="card-title">
-                            Company Name
+            <div className="row row-deck">
+                <div className="col-lg-4">
+                    <div className="card card-lg companyCard" id="company1Card">
+                        <div id="companyImg1" className="img-responsive img-responsive-16x9 card-img-top"></div>
+                        <div className="card-header">
+                            <div className="card-title">
+                                Company Name
+                            </div>
                         </div>
+                        <div className="card-body">
+                            Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
+                            Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
+                            or really anything the sponsor wants.
+                        </div>
+                        <div className="d-flex align-self-center">
+                            <a href="#" className="socialLinkLeftSide"><IconBrandInstagram></IconBrandInstagram></a>
+                            <a href="#"><IconWorldWww></IconWorldWww></a>
+                            <a href="#" className="socialLinkRightSide"><IconBrandLinkedin></IconBrandLinkedin></a>
+                        </div>
+                        <div className="socialFooterPadding"></div>
                     </div>
-                    <div className="card-body">
-                        Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
-                        Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
-                        or really anything the sponsor wants.
+                </div>
+                <div className="col-lg-4">
+                    <div className="card card-lg companyCard" id="company2Card">
+                        <div id="companyImg2" className="img-responsive img-responsive-16x9 card-img-top"></div>
+                        <div className="card-header">
+                            <div className="card-title">
+                                Company Name 2
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
+                            Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
+                            or really anything the sponsor wants.
+                        </div>
+                        <div className="d-flex align-self-center">
+                            <a href="#" className="socialLinkLeftSide"><IconBrandInstagram></IconBrandInstagram></a>
+                            <a href="#"><IconWorldWww></IconWorldWww></a>
+                            <a href="#" className="socialLinkRightSide"><IconBrandLinkedin></IconBrandLinkedin></a>
+                        </div>
+                        <div className="socialFooterPadding"></div>
                     </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                </div>
+                <div className="col-lg-4">
+                    <div className="card card-lg companyCard" id="company3Card">
+                        <div id="companyImg3" className="img-responsive img-responsive-16x9 card-img-top"></div>
+                        <div className="card-header">
+                            <div className="card-title">
+                                Company Name 3
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
+                            Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
+                            or really anything the sponsor wants.
+                        </div>
+                        <div className="d-flex align-self-center">
+                            <a href="#" className="socialLinkLeftSide"><IconBrandInstagram></IconBrandInstagram></a>
+                            <a href="#"><IconWorldWww></IconWorldWww></a>
+                            <a href="#" className="socialLinkRightSide"><IconBrandLinkedin></IconBrandLinkedin></a>
+                        </div>
+                        <div className="socialFooterPadding"></div>
                     </div>
                 </div>
             </div>
-            <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company2Card">
-                    <div id="companyImg2" className="img-responsive img-responsive-16x9 card-img-top"></div>
-                    <div className="card-header">
-                        <div className="card-title">
-                            Company Name 2
+            <div className="row row-deck">
+                <div className="col-lg-4">
+                    <div className="card card-lg companyCard" id="company4Card">
+                        <div id="companyImg4" className="img-responsive img-responsive-16x9 card-img-top"></div>
+                        <div className="card-header">
+                            <div className="card-title">
+                                Company Name 4
+                            </div>
                         </div>
-                    </div>
-                    <div className="card-body">
-                        Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
-                        Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
-                        or really anything the sponsor wants.
-                    </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                        <div className="card-body">
+                            Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
+                            Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
+                            or really anything the sponsor wants.
+                        </div>
+                        <div className="d-flex align-self-center">
+                            <a href="#" className="socialLinkLeftSide"><IconBrandInstagram></IconBrandInstagram></a>
+                            <a href="#"><IconWorldWww></IconWorldWww></a>
+                            <a href="#" className="socialLinkRightSide"><IconBrandLinkedin></IconBrandLinkedin></a>
+                        </div>
+                        <div className="socialFooterPadding"></div>
                     </div>
                 </div>
-            </div>
-            <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company3Card">
-                    <div id="companyImg3" className="img-responsive img-responsive-16x9 card-img-top"></div>
-                    <div className="card-header">
-                        <div className="card-title">
-                            Company Name 3
+                <div className="col-lg-4">
+                    <div className="card card-lg companyCard" id="company5Card">
+                        <div id="companyImg5" className="img-responsive img-responsive-16x9 card-img-top"></div>
+                        <div className="card-header">
+                            <div className="card-title">
+                                Company Name 5
+                            </div>
                         </div>
-                    </div>
-                    <div className="card-body">
-                        Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
-                        Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
-                        or really anything the sponsor wants.
-                    </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                        <div className="card-body">
+                            Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
+                            Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
+                            or really anything the sponsor wants.
+                        </div>
+                        <div className="d-flex align-self-center">
+                            <a href="#" className="socialLinkLeftSide"><IconBrandInstagram></IconBrandInstagram></a>
+                            <a href="#"><IconWorldWww></IconWorldWww></a>
+                            <a href="#" className="socialLinkRightSide"><IconBrandLinkedin></IconBrandLinkedin></a>
+                        </div>
+                        <div className="socialFooterPadding"></div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className="row row-deck">
-            <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company4Card">
-                    <div id="companyImg4" className="img-responsive img-responsive-16x9 card-img-top"></div>
-                    <div className="card-header">
-                        <div className="card-title">
-                            Company Name 4
+                <div className="col-lg-4">
+                    <div className="card card-lg companyCard" id="company6Card">
+                        <div id="companyImg6" className="img-responsive img-responsive-16x9 card-img-top"></div>
+                        <div className="card-header">
+                            <div className="card-title">
+                                Company Name 6
+                            </div>
                         </div>
-                    </div>
-                    <div className="card-body">
-                        Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
-                        Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
-                        or really anything the sponsor wants.
-                    </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company5Card">
-                    <div id="companyImg5" className="img-responsive img-responsive-16x9 card-img-top"></div>
-                    <div className="card-header">
-                        <div className="card-title">
-                            Company Name 5
+                        <div className="card-body">
+                            Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
+                            Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
+                            or really anything the sponsor wants.
                         </div>
-                    </div>
-                    <div className="card-body">
-                        Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
-                        Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
-                        or really anything the sponsor wants.
-                    </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-4">
-                <div className="card card-lg companyCard" id="company6Card">
-                    <div id="companyImg6" className="img-responsive img-responsive-16x9 card-img-top"></div>
-                    <div className="card-header">
-                        <div className="card-title">
-                            Company Name 6
+                        <div className="d-flex align-self-center">
+                            <a href="#" className="socialLinkLeftSide"><IconBrandInstagram></IconBrandInstagram></a>
+                            <a href="#"><IconWorldWww></IconWorldWww></a>
+                            <a href="#" className="socialLinkRightSide"><IconBrandLinkedin></IconBrandLinkedin></a>
                         </div>
-                    </div>
-                    <div className="card-body">
-                        Here lies a description of the company that has so generously chosen to sponsor our event and made this possible!
-                        Thank you for your support and there could be something else put here, for example a description of the services that they offer, a link to their website,
-                        or really anything the sponsor wants.
-                    </div>
-                    <div className="d-lg-block companySocialFooter">
-                        <a href="#" className="companySocialLink"><IconWorldWww></IconWorldWww></a>
-                        <a href="#" className="companySocialLink"><IconBrandInstagram></IconBrandInstagram></a>
-                        <a href="#" className="companySocialLink"><IconBrandFacebook></IconBrandFacebook></a>
-                        <a href="#" className="companySocialLink companySocialLinkCenter"><IconBrandYoutube></IconBrandYoutube></a>
-                        <a href="#" className="companySocialLink"><IconBrandX></IconBrandX></a>
-                        <a href="#" className="companySocialLink"><IconBrandTiktok></IconBrandTiktok></a>
+                        <div className="socialFooterPadding"></div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
