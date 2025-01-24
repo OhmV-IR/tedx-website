fb79aa3109f8bf5b15e1d4b4752299d76d3a46d7
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-24 13:42:19 -0500

    Adjust site menu open/close functions for mobile elements

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     useEffect(() => {
         alert(mobile())
         setIsMobile(mobile())
-    })
+    }) // site menu open/close icons don't want to click
     return (
         <html>
             <body>
@@ -28,94 +28,94 @@ export default function RootLayout({ children }) {
                         : <IconMenu2 stroke={2} size={44} id="menuIcon" onClick={OpenSiteMenu} color="#EB0028"></IconMenu2>
                     }
                     {isMobile
-                    ? <div id="siteMenuMobile">
-                        <a href="/">
-                            <div id="firstMenuElementMobile" className="menuElementMobile">
-                                <IconHome className="menuIconMobile" id="homeIcon" color="#EB0028"></IconHome>
-                                <h3 className="menuLabelMobile" id="homeLabel">Home</h3>
-                            </div>
-                        </a>
-                        <a href="/about">
-                            <div className="menuElementMobile">
-                                <IconInfoSquare className="menuIconMobile" id="aboutIcon" color="#EB0028"></IconInfoSquare>
-                                <h3 className="menuLabelMobile" id="aboutLabel">About TED</h3>
-                            </div>
-                        </a>
-                        <a href="/media">
-                            <div className="menuElementMobile">
-                                <IconPhoto className="menuIconMobile" id="mediaIcon" color="#EB0028"></IconPhoto>
-                                <h3 className="menuLabelMobile" id="mediaLabel">Media</h3>
-                            </div>
-                        </a>
-                        <a href="/sponsors">
-                            <div className="menuElementMobile">
-                                <IconCurrencyDollar className="menuIconMobile" id="sponsorsIcon" color="#EB0028"></IconCurrencyDollar>
-                                <h3 className="menuLabelMobile" id="sponsorsLabel">Sponsors</h3>
-                            </div>
-                        </a>
-                        <a href="/speakers">
-                            <div className="menuElementMobile">
-                                <IconSpeakerphone className="menuIconMobile" id="speakersIcon" color="#EB0028"></IconSpeakerphone>
-                                <h3 className="menuLabelMobile" id="speakersLabel">Speakers</h3>
-                            </div>
-                        </a>
-                        <a href="/schedule">
-                            <div className="menuElementMobile">
-                                <IconCalendarEvent className="menuIconMobile" id="scheduleIcon" color="#EB0028"></IconCalendarEvent>
-                                <h3 className="menuLabelMobile" id="scheduleLabel">Schedule</h3>
-                            </div>
-                        </a>
-                        <a href="/ourteam">
-                            <div className="menuElementMobile">
-                                <IconUsers className="menuIconMobile" id="ourTeamIcon" color="#EB0028"></IconUsers>
-                                <h3 className="menuLabelMobile" id="ourTeamLabel" onAnimationEnd={EndAnimationSiteMenu}>Our Team</h3>
-                            </div>
-                        </a>
-                    </div>
-                    : <div id="siteMenu">
-                        <a href="/">
-                            <div id="firstMenuElement" className="menuElement">
-                                <IconHome className="menuIcon" id="homeIcon" color="#EB0028"></IconHome>
-                                <h3 className="menuLabel" id="homeLabel">Home</h3>
-                            </div>
-                        </a>
-                        <a href="/about">
-                            <div className="menuElement">
-                                <IconInfoSquare className="menuIcon" id="aboutIcon" color="#EB0028"></IconInfoSquare>
-                                <h3 className="menuLabel" id="aboutLabel">About TED</h3>
-                            </div>
-                        </a>
-                        <a href="/media">
-                            <div className="menuElement">
-                                <IconPhoto className="menuIcon" id="mediaIcon" color="#EB0028"></IconPhoto>
-                                <h3 className="menuLabel" id="mediaLabel">Media</h3>
-                            </div>
-                        </a>
-                        <a href="/sponsors">
-                            <div className="menuElement">
-                                <IconCurrencyDollar className="menuIcon" id="sponsorsIcon" color="#EB0028"></IconCurrencyDollar>
-                                <h3 className="menuLabel" id="sponsorsLabel">Sponsors</h3>
-                            </div>
-                        </a>
-                        <a href="/speakers">
-                            <div className="menuElement">
-                                <IconSpeakerphone className="menuIcon" id="speakersIcon" color="#EB0028"></IconSpeakerphone>
-                                <h3 className="menuLabel" id="speakersLabel">Speakers</h3>
-                            </div>
-                        </a>
-                        <a href="/schedule">
-                            <div className="menuElement">
-                                <IconCalendarEvent className="menuIcon" id="scheduleIcon" color="#EB0028"></IconCalendarEvent>
-                                <h3 className="menuLabel" id="scheduleLabel">Schedule</h3>
-                            </div>
-                        </a>
-                        <a href="/ourteam">
-                            <div className="menuElement">
-                                <IconUsers className="menuIcon" id="ourTeamIcon" color="#EB0028"></IconUsers>
-                                <h3 className="menuLabel" id="ourTeamLabel" onAnimationEnd={EndAnimationSiteMenu}>Our Team</h3>
-                            </div>
-                        </a>
-                    </div>
+                        ? <div id="siteMenuMobile">
+                            <a href="/">
+                                <div id="firstMenuElementMobile" className="menuElementMobile">
+                                    <IconHome className="menuIconMobile" id="homeIcon" color="#EB0028"></IconHome>
+                                    <h3 className="menuLabelMobile" id="homeLabel">Home</h3>
+                                </div>
+                            </a>
+                            <a href="/about">
+                                <div className="menuElementMobile">
+                                    <IconInfoSquare className="menuIconMobile" id="aboutIcon" color="#EB0028"></IconInfoSquare>
+                                    <h3 className="menuLabelMobile" id="aboutLabel">About TED</h3>
+                                </div>
+                            </a>
+                            <a href="/media">
+                                <div className="menuElementMobile">
+                                    <IconPhoto className="menuIconMobile" id="mediaIcon" color="#EB0028"></IconPhoto>
+                                    <h3 className="menuLabelMobile" id="mediaLabel">Media</h3>
+                                </div>
+                            </a>
+                            <a href="/sponsors">
+                                <div className="menuElementMobile">
+                                    <IconCurrencyDollar className="menuIconMobile" id="sponsorsIcon" color="#EB0028"></IconCurrencyDollar>
+                                    <h3 className="menuLabelMobile" id="sponsorsLabel">Sponsors</h3>
+                                </div>
+                            </a>
+                            <a href="/speakers">
+                                <div className="menuElementMobile">
+                                    <IconSpeakerphone className="menuIconMobile" id="speakersIcon" color="#EB0028"></IconSpeakerphone>
+                                    <h3 className="menuLabelMobile" id="speakersLabel">Speakers</h3>
+                                </div>
+                            </a>
+                            <a href="/schedule">
+                                <div className="menuElementMobile">
+                                    <IconCalendarEvent className="menuIconMobile" id="scheduleIcon" color="#EB0028"></IconCalendarEvent>
+                                    <h3 className="menuLabelMobile" id="scheduleLabel">Schedule</h3>
+                                </div>
+                            </a>
+                            <a href="/ourteam">
+                                <div className="menuElementMobile">
+                                    <IconUsers className="menuIconMobile" id="ourTeamIcon" color="#EB0028"></IconUsers>
+                                    <h3 className="menuLabelMobile" id="ourTeamLabel" onAnimationEnd={EndAnimationSiteMenu}>Our Team</h3>
+                                </div>
+                            </a>
+                        </div>
+                        : <div id="siteMenu">
+                            <a href="/">
+                                <div id="firstMenuElement" className="menuElement">
+                                    <IconHome className="menuIcon" id="homeIcon" color="#EB0028"></IconHome>
+                                    <h3 className="menuLabel" id="homeLabel">Home</h3>
+                                </div>
+                            </a>
+                            <a href="/about">
+                                <div className="menuElement">
+                                    <IconInfoSquare className="menuIcon" id="aboutIcon" color="#EB0028"></IconInfoSquare>
+                                    <h3 className="menuLabel" id="aboutLabel">About TED</h3>
+                                </div>
+                            </a>
+                            <a href="/media">
+                                <div className="menuElement">
+                                    <IconPhoto className="menuIcon" id="mediaIcon" color="#EB0028"></IconPhoto>
+                                    <h3 className="menuLabel" id="mediaLabel">Media</h3>
+                                </div>
+                            </a>
+                            <a href="/sponsors">
+                                <div className="menuElement">
+                                    <IconCurrencyDollar className="menuIcon" id="sponsorsIcon" color="#EB0028"></IconCurrencyDollar>
+                                    <h3 className="menuLabel" id="sponsorsLabel">Sponsors</h3>
+                                </div>
+                            </a>
+                            <a href="/speakers">
+                                <div className="menuElement">
+                                    <IconSpeakerphone className="menuIcon" id="speakersIcon" color="#EB0028"></IconSpeakerphone>
+                                    <h3 className="menuLabel" id="speakersLabel">Speakers</h3>
+                                </div>
+                            </a>
+                            <a href="/schedule">
+                                <div className="menuElement">
+                                    <IconCalendarEvent className="menuIcon" id="scheduleIcon" color="#EB0028"></IconCalendarEvent>
+                                    <h3 className="menuLabel" id="scheduleLabel">Schedule</h3>
+                                </div>
+                            </a>
+                            <a href="/ourteam">
+                                <div className="menuElement">
+                                    <IconUsers className="menuIcon" id="ourTeamIcon" color="#EB0028"></IconUsers>
+                                    <h3 className="menuLabel" id="ourTeamLabel" onAnimationEnd={EndAnimationSiteMenu}>Our Team</h3>
+                                </div>
+                            </a>
+                        </div>
                     }
                     {children}
                     {siteMenuEnabled
@@ -131,41 +131,79 @@ export default function RootLayout({ children }) {
     function OpenSiteMenu() {
         console.log("Opening site menu");
         if (!siteMenuEnabled) {
-            let element = document.getElementById("siteMenu");
-            element.style.display = "block";
-            for (let i = 0; i < element.children.length; i++) {
-                let currentMenuItem = element.children.item(i).children.item(0);
-                currentMenuItem.children.item(0).style.animation = "menu-openicon 0.4s linear 0s 1 normal";
-                currentMenuItem.children.item(1).style.animation = "menu-openlabel 0.4s linear 0s 1 normal";
+            if (isMobile) {
+                let element = document.getElementById("siteMenuMobile");
+                element.style.display = "block";
+                for (let i = 0; i < element.children.length; i++) {
+                    let currentMenuItem = element.children.item(i).children.item(0);
+                    currentMenuItem.children.item(0).style.animation = "menu-openicon 0.4s linear 0s 1 normal";
+                    currentMenuItem.children.item(1).style.animation = "menu-openlabel 0.4s linear 0s 1 normal";
+                }
+                let menuIcon = document.getElementById("menuIcon");
+                menuIcon.style.animation = "menuIcon-shrink 0.4s linear 0s 1 normal";
+                let roots = document.getElementsByClassName("divRoot");
+                for (let i = 0; i < roots.length; i++) {
+                    roots[i].classList.add('hide');
+                }
             }
-            let menuIcon = document.getElementById("menuIcon");
-            menuIcon.style.animation = "menuIcon-shrink 0.4s linear 0s 1 normal";
-            let roots = document.getElementsByClassName("divRoot");
-            for (let i = 0; i < roots.length; i++) {
-                roots[i].classList.add('hide');
+            else {
+                let element = document.getElementById("siteMenu");
+                element.style.display = "block";
+                for (let i = 0; i < element.children.length; i++) {
+                    let currentMenuItem = element.children.item(i).children.item(0);
+                    currentMenuItem.children.item(0).style.animation = "menu-openicon 0.4s linear 0s 1 normal";
+                    currentMenuItem.children.item(1).style.animation = "menu-openlabel 0.4s linear 0s 1 normal";
+                }
+                let menuIcon = document.getElementById("menuIcon");
+                menuIcon.style.animation = "menuIcon-shrink 0.4s linear 0s 1 normal";
+                let roots = document.getElementsByClassName("divRoot");
+                for (let i = 0; i < roots.length; i++) {
+                    roots[i].classList.add('hide');
+                }
             }
         }
     }
     function CloseSiteMenu() {
         console.log("closing site menu")
         if (siteMenuEnabled) {
-            let element = document.getElementById("siteMenu");
-            for (let i = 0; i < element.children.length; i++) {
-                let currentMenuItem = element.children.item(i).children.item(0);
-                currentMenuItem.children.item(0).style.animation = "menu-closeicon 0.4s linear 0s 1 normal";
-                currentMenuItem.children.item(1).style.animation = "menu-closelabel 0.4s linear 0s 1 normal";
+            if (isMobile) {
+                let element = document.getElementById("siteMenuMobile");
+                for (let i = 0; i < element.children.length; i++) {
+                    let currentMenuItem = element.children.item(i).children.item(0);
+                    currentMenuItem.children.item(0).style.animation = "menu-closeicon 0.4s linear 0s 1 normal";
+                    currentMenuItem.children.item(1).style.animation = "menu-closelabel 0.4s linear 0s 1 normal";
+                }
+                let menuIcon = document.getElementById("menuIcon");
+                menuIcon.style.animation = "menuIcon-shrink 0.4s linear 0s 1 normal";
+                let roots = document.getElementsByClassName("divRoot");
+                for (let i = 0; i < roots.length; i++) {
+                    roots[i].classList.remove('hide');
+                }
             }
-            let menuIcon = document.getElementById("menuIcon");
-            menuIcon.style.animation = "menuIcon-shrink 0.4s linear 0s 1 normal";
-            let roots = document.getElementsByClassName("divRoot");
-            for (let i = 0; i < roots.length; i++) {
-                roots[i].classList.remove('hide');
+            else {
+                let element = document.getElementById("siteMenu");
+                for (let i = 0; i < element.children.length; i++) {
+                    let currentMenuItem = element.children.item(i).children.item(0);
+                    currentMenuItem.children.item(0).style.animation = "menu-closeicon 0.4s linear 0s 1 normal";
+                    currentMenuItem.children.item(1).style.animation = "menu-closelabel 0.4s linear 0s 1 normal";
+                }
+                let menuIcon = document.getElementById("menuIcon");
+                menuIcon.style.animation = "menuIcon-shrink 0.4s linear 0s 1 normal";
+                let roots = document.getElementsByClassName("divRoot");
+                for (let i = 0; i < roots.length; i++) {
+                    roots[i].classList.remove('hide');
+                }
             }
         }
     }
     function EndAnimationSiteMenu() {
         if (siteMenuEnabled) {
-            document.getElementById("siteMenu").style.display = "none";
+            if (isMobile) {
+                document.getElementById("siteMenuMobile").style.display = "none";
+            }
+            else {
+                document.getElementById("siteMenu").style.display = "none";
+            }
         }
         else {
             document.getElementById("menuIcon").style.animation = "menuIcon-expand 0.4s linear 0s 1 normal";
