c21f95ec76080087815bbf7c137a4eeca13cc573
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-03-07 09:23:39 -0500

    test

--- src/app/api/unsubscribe/route.js
+++ src/app/api/unsubscribe/route.js
@@ -1,11 +1,26 @@
 const dotenv = require('dotenv')
 dotenv.config();
+import { env } from "node:process";
 const { CosmosClient } = require("@azure/cosmos");
 const sqlstring = require('sqlstring');
 const endpoint = process.env.DBENDPOINT;
 const key = process.env.DBKEY;
+const dbid = process.env.DBID;
+const containerid = process.env.CONTAINERID;
+if(key == ""){
+    key = env.DBKEY
+}
+if(endpoint == ""){
+    endpoint = env.DBENDPOINT
+}
+if(dbid == ""){
+    dbid = env.DBID;
+}
+if(containerid == ""){
+    containerid = env.CONTAINERID;
+}
 const client = new CosmosClient({ endpoint, key });
-const { container } = await client.database(process.env.DBID).containers.createIfNotExists({ id: process.env.CONTAINERID });
+const { container } = await client.database(dbid).containers.createIfNotExists({ id: containerid });
 export async function POST(request) {
     var email = request.nextUrl.searchParams.get('email');
     if (!email.match("[a-z0-9].*\@[a-z0-9].*\..*")) {
