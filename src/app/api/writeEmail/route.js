ab90385ae7fbc85ffa55c6d3d5540b7596a7573c
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-05-08 01:19:06 -0400

    bugfix

--- src/app/api/writeEmail/route.js
+++ src/app/api/writeEmail/route.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const { CosmosClient } = require("@azure/cosmos");
-const endpoint = process.env.DB_ENDPOINT;
-const key = process.env.DB_KEY;
+const endpoint = process.env.DBENDPOINT;
+const key = process.env.DBKEY;
 const client = new CosmosClient({endpoint, key});
-const { container } = await client.database(process.env.DB_ID).containers.createIfNotExists({id: process.env.CONTAINER_ID});
+const { container } = await client.database(process.env.DBID).containers.createIfNotExists({id: process.env.CONTAINERID});
 export async function POST(request){
     var email = request.nextUrl.searchParams.get('email');
     if(!email.match("[a-z0-9].*\@[a-z0-9].*\..*")){
